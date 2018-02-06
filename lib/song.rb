52e83a23700d1e17008e83111e21d067e0b4e2f4
Brendan O'Dea <brendan.ode4@gmail.com>
2018-02-06 03:34:26 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,5 +2,17 @@ class Song
   @@count = 0
   @@artists = []
   @@genres = []
-end
+  @@genre_count = {}
+  @@artist_count = {}
 
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @count += 1
+  end
+
+  
+end
